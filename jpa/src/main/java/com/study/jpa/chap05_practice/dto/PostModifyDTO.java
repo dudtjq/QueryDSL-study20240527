a2a440022160cc8cf4d78f3b7fb36d34c6218f4e
dudtjq <dudtjq0415@naver.com>
2024-05-16 15:58:20 +0900

    종합 연습 예제(게시글 수정, 삭제) 처리, swagger ui(API 명세서) 사용법

---
+++ jpa/src/main/java/com/study/jpa/chap05_practice/dto/PostModifyDTO.java
@@ -0,0 +1,26 @@
+package com.study.jpa.chap05_practice.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Setter @Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor @NoArgsConstructor
+@Builder
+public class PostModifyDTO {
+
+    @NotBlank
+    @Size(min = 1, max = 20)
+    private String title;
+
+    private String content;
+
+    @NotNull // 공백이나 빈 문자열이 들어올 수 없는 타입은 NotNull 로 선언
+    private Long postNo;
+
+
+
+}
--- jpa/src/main/java/com/study/jpa/chap05_practice/service/PostService.java
+++ jpa/src/main/java/com/study/jpa/chap05_practice/service/PostService.java
@@ -62,11 +62,16 @@ public class PostService {
 
     public PostDetailResponseDTO getDetail(Long id) throws Exception {
 
-        Post post = postRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException(id + "번 게시물이 존재하지 않습니다."));
+        Post post = getPost(id);
 
         return new PostDetailResponseDTO(post);
 
+    }
+
+    private Post getPost(Long id) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(id + "번 게시물이 존재하지 않습니다."));
+        return post;
     }
 
     public PostDetailResponseDTO insert(PostCreateDTO dto) throws Exception {
@@ -107,6 +112,28 @@ public class PostService {
 
         //저장된 insert 요청한 게시물 정보를 DTO 로 변환해서 리턴
         return new PostDetailResponseDTO(saved);
+
+    }
+
+    public PostDetailResponseDTO modify(PostModifyDTO dto) {
+    
+        // 수정 전 데이터를 조회
+        Post postEntity = getPost(dto.getPostNo());
+
+        // 수정 시작
+        postEntity.setTitle(dto.getTitle());
+        postEntity.setContent(dto.getContent());
+
+        // 수정 완료
+        Post modifyPost = postRepository.save(postEntity);
+
+        return new PostDetailResponseDTO(modifyPost);
+
+    }
+
+    public void delete(Long id) {
+
+        postRepository.deleteById(id);
 
     }
 }
