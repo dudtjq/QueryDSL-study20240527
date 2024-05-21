dd4eda9be142db007e0adcb9860aeb88bae54b02
dudtjq <dudtjq0415@naver.com>
2024-05-21 13:22:28 +0900

    JWT 검증을 위한 filter 생성 및 spring security 설정

--- todo/src/main/java/com/example/todo/config/WebSecurityConfig.java
+++ todo/src/main/java/com/example/todo/config/WebSecurityConfig.java
@@ -1,16 +1,26 @@
 package com.example.todo.config;
 
+import com.example.todo.filter.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
 
     // 시큐리티 기본 설정 (권한처리, 초기 로그인 화면 없애기 ....)
     @Bean // 라이브러리 클래스 같은 내가 만들지 않은 객체를 등록해서 주입받기 위한 아노테이션.
@@ -18,8 +28,32 @@ public class WebSecurityConfig {
 
         http
                 .csrf(csrfConfig -> csrfConfig.disable()) // CSRF 토큰공격을 방지하기 위한 장치 해제.
+                .cors(Customizer.withDefaults())
+                // 세션 관리 상태를 STATELESS 로 성정해서 spring security 가 제공하는 세션 생성 및 관리 기능
+                // 사용하지 않겠다
+                .sessionManagement(SessionManagement ->
+                        SessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // spring 에서 제공하는 기본 로그인 폼 사용 안함, http 기반 기본 인증도 안 쓰겠단
+                .formLogin(form -> form.disable())
+                .httpBasic(AbstractHttpConfigurer::disable)
+                // 우리가 만든 jwtAuthFilter 를 UsernamePasswordAuthenticationFilter
+                // 보다 먼저 동작하도록 설정
+                // security 를 사용하면, 서버가 가동될 때 기본적으로 제공하는 여러가지 필터가 세팅이
+                // 되는데, jwtAuthFilter 를 먼저 배치헤서, jwtAuthFilter 가 통과하면 인증이 완료가
+                // 되도록 처리
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests.requestMatchers("/**").permitAll()); // 모든 요청에 대해 인증을 하지 않겠다.
+                        authorizeRequests
+                                // '/api/todos' 라는 요청이 post 로 들어오고, Role 값이 ADMIN 인 경우
+                                // 권한 검사 없이 허용하겠다
+                                //.requestMatchers(HttpMethod.POST, "/api/todos")
+                                //.hasRole("ADMIN")
+                                // /api/auth/ 로 시작하는 요청과 / 요청은 권한 검사 없이 허용 하겠다
+                                .requestMatchers("/", "/api/auth/**")
+                                .permitAll()
+                                // 위에서 따로 설정하지 않은 나머지 요청들은 권한 검사가 필요함
+                                .anyRequest().authenticated()
+                );
 
         return http.build();
     }
@@ -29,4 +63,5 @@ public class WebSecurityConfig {
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
